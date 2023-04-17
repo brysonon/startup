7e20f18454d1a17957b65bd844fa59d85844df98
brysonon <on.bryson@gmail.com>
2023-04-17 16:52:52 -0500

    Created view components and updated App.jsx and App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+
+  // Asynchronously determine if the user is authenticated by calling the service
+  const [authState, setAuthState] = React.useState(AuthState.Unknown);
+  React.useEffect(() => {
+    if (userName) {
+      fetch(`/api/user/${userName}`)
+        .then((response) => {
+          if (response.status === 200) {
+            return response.json();
+          }
+        })
+        .then((user) => {
+          const state = user?.authenticated ? AuthState.Authenticated : AuthState.Unauthenticated;
+          setAuthState(state);
+        });
+    } else {
+      setAuthState(AuthState.Unauthenticated);
+    }
+  }, [userName]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <header className="flex-wrap align-items-center justify-content-center justify-content-md-between">
+      <h1>
+        <span className="fw-bold text-dark">Caregiver Tool</span>
+      </h1>
+      <ul className="nav nav-pills p-1 mx-3">
+        <li className='nav-item'>
+          <NavLink className='nav-link' to=''>
+            Login
+          </NavLink>
+        </li>
+        {authState === AuthState.Authenticated && (
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='play'>
+              Play
+            </NavLink>
+          </li>
+        )}
+        {authState === AuthState.Authenticated && (
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='scores'>
+              Scores
+            </NavLink>
+          </li>
+        )}
+        <li className='nav-item'>
+          <NavLink className='nav-link' to='about'>
+            About
+          </NavLink>
+        </li>
+      </ul>
+    </header>
+
+      <footer className="flex-wrap justify-content-between align-items-center">
+        <div className="d-flex align-items-center">
+          <a className='text-muted' href="https://github.com/brysonon/startup">Bryson On</a>
+        </div>
+      </footer>
+    </body>
   );
 }
 
---
+++ src/vitals/vitals.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Vitals() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>login displayed here</div>
+    </main>
+  );
+}
